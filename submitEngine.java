69b351b7f05c9bbbbc43ef0a327528823f9b6f12
wittch <hwan95212@naver.com>
2021-01-26 17:24:39 +0900

    create submitEngine.java, case 1. case 2.

--- submitEngine.java
+++ submitEngine.java
@@ -1,4 +1,4 @@
-@RequestMapping(value = "/spring/submitEngine", produces = "application/json; charset=utf8", method = RequestMethod.POST)
+	@RequestMapping(value = "/spring/submitEngine", produces = "application/json; charset=utf8", method = RequestMethod.POST)
 	public ResponseEntity<String> submitEngine(String SubNum, String Pnum, int tccnt, Object Pcode) {
 		// make Body
 		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
@@ -11,6 +11,8 @@
 		
 		String tempurl;
 		
+		
+		//방법 1.
 		/*외부에 추가해야 할 코드
 		 * static Queue<String> serverqueue=new LinkedList<String>();
 		static Queue<String> clientqSubNum=new LinkedList<String>();
@@ -44,19 +46,67 @@
 				params.add("Pnum", clientqPnum.poll());
 				params.add("Pcode", clientqPcode.poll());
 			}
-			tempurl = eurl+"/judger-engine/"+serverqueue.poll();
-			
-			serverqueue.add(tempurl);
+			tempurl = serverqueue.poll();
 			
 		}
 		
 		
-		// make Entity
+		//방법 2.
+		
+		
+		/*Queue<HttpEntity<MultiValueMap<String, String>>> entity_ = new LinkedList<HttpEntity<MultiValueMap<String, String>>>();
+		 * 
+		 */
+		
+		/*외부에 추가해야 할 코드
+		 * static Queue<String> serverqueue=new LinkedList<String>();
+		static Queue<HttpEntity<MultiValueMap<String, String>>> entity_ = new LinkedList<HttpEntity<MultiValueMap<String, String>>>();
+		
+		serverqueue.add("1");
+		serverqueue.add("2");
+		serverqueue.add("3");
+		serverqueue.add("4");
+		serverqueue.add("5");
+		serverqueue.add("6");*/
+	
+		
+		
+		Queue<HttpEntity<MultiValueMap<String, String>>> entity_ = new LinkedList<HttpEntity<MultiValueMap<String, String>>>();
+
+		
 		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity(params, headers); 
 		RestTemplate rt = new RestTemplate();
+		ResponseEntity<String> rt_;
 		
 		
-		/*String url = eurl+"/judger-engine/1";*/
-		return rt.exchange(tempurl, HttpMethod.POST, entity, String.class);
+		
+		params.add("SubNum", "[제출 번호]");
+		params.add("Pnum", "[문제 번호]");
+		params.add("Pcode", "[제출 코드]");
+		
+		
+		
+		
+		if(serverqueue.isEmpty()==true)
+		{
+			entity_.add(entity);
+		}
+		else {
+			
+			tempurl = serverqueue.poll();
+			
+			if(entity_.isEmpty()==false)
+			{
+				rt_=rt.exchange(eurl+"/judger-engine/"+tempurl, HttpMethod.POST, entity_.poll(), String.class);
+				serverqueue.add(tempurl);
+			}
+			else {
+				rt_=rt.exchange(eurl+"/judger-engine/"+tempurl, HttpMethod.POST, entity, String.class);	
+				serverqueue.add(tempurl);
+			}
+		}
+		
+		
+		return rt_;
 		
 	}
