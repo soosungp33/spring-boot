8a1461ce1409375e170209b69a281574b868d0db
show7805 <backsm2002@naver.com>
2021-01-27 18:36:15 +0900

    1st docker test

---
+++ Sample1/src/main/java/com/sample/webb/ProblemList.java
@@ -0,0 +1,70 @@
+package com.sample.webb;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ProblemList {
+	final static String furl = HomeController.furl;
+	
+	
+	@RequestMapping(value = "/spring/problemdata", method = RequestMethod.GET)
+	@CrossOrigin("*")
+	public ResponseEntity<String> center(HttpServletResponse response) throws ParseException {
+		// get Problem data from Flask
+		ResponseEntity<String> resp = reqUserInfo();
+		JSONParser UserSolvInfo = new JSONParser();
+		JSONArray USIob = (JSONArray) UserSolvInfo.parse(resp.getBody().toString());
+		JSONArray ja = new JSONArray();
+		int size=USIob.size();
+		int i=0;
+		while(true) {
+			ja.add(makePData((JSONArray) USIob.get(i)));
+			++i;
+			if(i==size) break;
+		}
+		
+		// return to React
+		String jsonstr = ja.toJSONString();
+		JSONObject jsmain = new JSONObject();
+		jsmain.put("Problems", ja);
+		String jsstr = jsmain.toJSONString();
+		return ResponseEntity.ok(jsstr);
+	}
+	
+	public JSONObject makePData(JSONArray j) {
+		int pnum = Integer.parseInt(j.get(0).toString());
+		String name = j.get(1).toString();
+		String solved = j.get(2).toString();
+		JSONObject js = new JSONObject();
+		js.put("Pnum", pnum);
+		js.put("Pname", name);
+		js.put("Solved",solved);
+		return js;
+	}
+	
+	public ResponseEntity<String> reqUserInfo() {
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers); 
+		RestTemplate rt = new RestTemplate();
+		ResponseEntity<String> response = rt.exchange(furl, HttpMethod.GET, entity, String.class);
+		return response;
+	}
+}
---
+++ Sample1/src/main/java/com/sample/webb/SubmitCode.java
@@ -0,0 +1,133 @@
+package com.sample.webb;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SubmitCode {
+	
+	static HttpHeaders headers = new HttpHeaders();
+	static final String furl = HomeController.furl;
+	static final String eurl = HomeController.eurl;
+	static int SubmitNum = 1;
+	final static int capacity = 1;
+	static int curengine = 0;
+	static ArrayBlockingQueue<Integer> enq = new ArrayBlockingQueue<Integer>(capacity);
+	
+	@RequestMapping(value = "/spring/submitcode", produces = "application/json; charset=utf8", method = RequestMethod.POST)
+	@CrossOrigin("*")
+	public ResponseEntity<String> submitcode(@RequestBody String body) throws ParseException, UnsupportedEncodingException, InterruptedException{
+		headers.add("Content-Type", "application/json");
+		// from React
+		JSONParser jp = new JSONParser();
+		JSONObject js = (JSONObject) jp.parse(body);
+		String Pnum = js.get("Pnum").toString();
+		Object Pcode = js.get("code");
+		System.out.println(js);
+		
+		//to Engine, Flask
+		ResponseEntity<String> fresp1 = submitCode(SubmitNum, Integer.parseInt(Pnum));
+		int tc_cnt=5;
+		cycle();
+		System.out.println("current engine number: " + curengine);
+		enq.put(curengine);
+		//ResponseEntity<String> eresp = submitEngine(Integer.toString(SubmitNum), Pnum, tc_cnt, Pcode);
+		enq.poll();
+		//System.out.println("engine response: " + eresp.getBody().toString());
+		//String result = eresp.getBody().toString();
+		//ResponseEntity<String> fresp2 = receiveResult(Integer.toString(SubmitNum), result);
+		
+		// response to React
+		JSONObject jsmain = new JSONObject();
+		JSONObject jsarr = new JSONObject();
+		JSONArray jamain = new JSONArray();
+		jsarr.put("SubNum", Integer.toString(SubmitNum));
+		jsarr.put("Result", "Accepted");
+		jamain.add(jsarr);
+		jsmain.put("Problems", jamain);
+		System.out.println(jsmain);
+		SubmitNum++;
+		return ResponseEntity.ok(jsmain.toJSONString());
+	}
+	
+	
+	public ResponseEntity<String> submitCode(int SubNum, int Pnum) {
+		// make Body
+		MultiValueMap<String, Integer> params = new LinkedMultiValueMap<>();
+		params.add("SubNum", SubNum);
+		params.add("Pnum", Pnum);
+		System.out.println("Flask data: "+params.toString());
+		
+		// make Entity
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity(params, headers); 
+		RestTemplate rt = new RestTemplate();
+		String url = furl+"/flask_Submit";
+		return rt.exchange(url, HttpMethod.POST, entity, String.class);
+		
+	}
+	
+	public ResponseEntity<String> submitEngine(String SubNum, String Pnum, int tccnt, Object Pcode) throws InterruptedException {
+		
+		
+		// make Body
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		System.out.println(Pcode);
+		params.add("SubNum", SubNum);
+		params.add("Pnum", Pnum);
+		params.add("tc-cnt", tccnt);
+		params.add("Pcode", Pcode);
+		System.out.println("Engine form: "+params.toString());
+		
+		// make Entity
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity(params, headers); 
+		RestTemplate rt = new RestTemplate();
+		String url = eurl+"/judger-engine/"+Integer.toString(curengine);
+		return rt.exchange(url, HttpMethod.POST, entity, String.class);
+		
+	}
+	
+	public ResponseEntity<String> receiveResult(String SubNum, String Result) {
+		// make Body
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("SubNum", SubNum);
+		params.add("Result", Result);
+		JSONObject js = new JSONObject();
+		JSONArray ja1 = new JSONArray();
+		JSONArray ja2 = new JSONArray();
+		ja1.add(Integer.parseInt(SubNum));
+		ja2.add(Result);
+		js.put("SubNum", ja1);
+		js.put("Result", ja2);
+		System.out.println("pass result to flask: " + js.toJSONString());
+		
+		// make Entity
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity(js, headers); 
+		RestTemplate rt = new RestTemplate();
+		String url = furl+"/flask_submit_result";
+		return rt.exchange(url, HttpMethod.POST, entity, String.class);
+		
+	}
+	static void cycle() {
+		if(curengine==capacity) {
+			curengine = 1;
+		}
+		else curengine++;
+	}
+}
