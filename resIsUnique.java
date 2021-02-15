867fba01d395b8ebaf60dbe2e7bbccd75bb46c71
dayoung <ekffkddl0529@naver.com>
2021-02-15 20:03:51 +0900

    create resIsUnique

---
+++ resIsUnique.java
@@ -0,0 +1,23 @@
+package com.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+@RestController
+public class resIsUnique {
+	
+	@RequestMapping(value="/spring/isunique", method = RequestMethod.POST)
+	public JSONObject resIsUnique(String id){
+		JSONObject obj = new JSONObject();
+		
+		obj.put("isUnique", "True");
+		
+		return obj;
+	}
+
+}
